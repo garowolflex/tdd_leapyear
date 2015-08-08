8a2c0b9a71a3dd4fca62babc3977c00eb7f2819c
garowolflex <galdstrings@gmail.com>
2015-08-08 17:07:24 +0900

    スペックと実装

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,5 +1,10 @@
-require "leap_year/version"
+require 'date'
 
-module LeapYear
-  # Your code goes here...
+class Date
+  def leap_year?
+    if year % 100 == 0
+      return year % 400 == 0
+    end
+    year % 4 == 0
+  end
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
 
-describe LeapYear do
-  it 'has a version number' do
-    expect(LeapYear::VERSION).not_to be nil
-  end
+describe Date do
+  describe '#leap_year?' do
+    context '4で割り切れない年のとき' do
+      it 'returns false' do
+        expect(Date.strptime('2015', '%Y').leap_year?).to eq false
+      end
+    end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+    context '4で割り切れる年のとき' do
+      it 'returns true if 100では割り切れない' do
+        expect(Date.strptime('2016', '%Y').leap_year?).to eq true
+      end
+
+      it 'returns false if 100で割り切れるが、400で割り切れない' do
+        expect(Date.strptime('2100', '%Y').leap_year?).to eq false
+        expect(Date.strptime('2200', '%Y').leap_year?).to eq false
+        expect(Date.strptime('2300', '%Y').leap_year?).to eq false
+      end
+
+      it 'returns true if 400で割り切れる' do
+        expect(Date.strptime('2000', '%Y').leap_year?).to eq true
+        expect(Date.strptime('2400', '%Y').leap_year?).to eq true
+      end
+    end
   end
 end
